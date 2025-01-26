f540d1930492d9592b00e67faf18f41470f851f9
Smah-amen <samahamen20001@gmail.com>
2025-01-26 23:40:07 +0200

    refactor Login component layout and form; update Tailwind CSS theme to dark

--- Front end/src/pages/login/Login.jsx
+++ Front end/src/pages/login/Login.jsx
@@ -2,12 +2,50 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
-        <h1 className="text-4xl font-semibold text-center text-gray-200">Login </h1>
-        <img src="" alt=""
-        loading="lazy"
-        />
+    <div className="container mx-auto flex items-center justify-center h-screen">
+      <div className=" flex flex-col items-center justify-center min-w-80  mx-auto">
+        <div className="w-full p-6 rounded-lg shadow-md bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
+          <h1 className="text-4xl font-semibold text-center text-gray-200">
+            Login
+            {/* <span className="text-blue-500">Chat app </span> */}
+          </h1>
+          <form>
+            <div className="">
+              <label className="label p-2 ">
+                <span className="text-base label-text ">username</span>
+              </label>
+              <input
+                className=" w-full h-10 input input-bordered"
+                type="text"
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="mt-4">
+              <label className="label p-2">
+                <span className="text-base label-text">password</span>
+              </label>
+              <input
+                className=" w-full h-10 input input-bordered"
+                type="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <a
+                href="/register"
+                className="text-blue-400 hover:text-blue-600 text-sm underline "
+              >
+                don't have an account
+              </a>
+              {/* <a href="#" className="text-blue-500">
+                Forgot Password?
+              </a> */}
+            </div>
+            <div className="mt-4 flex items-center justify-center">
+              <button className=" btn hover:bg-blue-400  text-white btn-sm mt-2">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
