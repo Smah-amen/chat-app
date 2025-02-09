d75677aef18601eeaea82c9f0cef0f0f14d26ac1
Smah-amen <samahamen20001@gmail.com>
2025-02-09 19:41:03 +0200

    refactor Conversation component to integrate conversation selection and display user profile picture and name

--- Front end/src/component/sidebar/Conversation.jsx
+++ Front end/src/component/sidebar/Conversation.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import useConversation from "../../storeZustand/useConversation";
 
-const Conversation = () => {
+const Conversation = ({ conversation, emoji, lastIndx }) => {
+  const {selectedConversation , setSelectedConversation} = useConversation()
+
+  const isSelacted = selectedConversation?._id === conversation._id
   return (
     <>
-    <div className=' flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
-      <div className='avatar online'>
-        <div className='w-12 rounded-full'>
-          <img src='icon.jpeg' alt='user' />
-
+      <div className= {`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+        ${isSelacted ? "bg-sky-500" : "" }
+        `}
+        onClick={() => setSelectedConversation(conversation) }
+        >
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePic}
+             alt="user" />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="text-sm font-medium">{conversation.fullName} </p>
+            <p className="text-lg text-gray-400">{emoji} </p>
+          </div>
         </div>
       </div>
-      <div className='flex flex-col flex-1'>
-        <div className='flex gap-3 justify-between'> 
-        <p className='text-sm font-medium'>John Doe</p>
-        <p className='text-xs text-gray-400'>Last seen: 5 minutes ago</p>
-        </div>
-      </div>
-    </div>
-<div className='divider my-0 py-0 h-1' />
+      {lastIndx&& <div className="divider my-0 py-0 h-1" />}
     </>
-  )
-}
+  );
+};
 
 export default Conversation;
