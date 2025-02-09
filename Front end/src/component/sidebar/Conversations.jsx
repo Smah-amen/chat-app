269f4d0bb89c5dbc67da0eeba3fbf02acff1fd9c
Smah-amen <samahamen20001@gmail.com>
2025-02-09 19:45:35 +0200

    add Zustand for state management; update Conversations component to fetch and display conversations with loading state

--- Front end/src/component/sidebar/Conversations.jsx
+++ Front end/src/component/sidebar/Conversations.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Conversation from "./Conversation";
+import useGetConversation from "../../hooks/useGetConversation";
+import { getRandomEmoji } from "../../utils/emojis";
 
-function Conversations() {
+const Conversations = () => {
+  const { loading, conversations } = useGetConversation();
+  // console.log("conversations", conversations);
+
   return (
-    <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-     
+    <div className="py-2 flex flex-col  min-w-[450px] overflow-auto">
+      {conversations && conversations.length > 0 ? (
+        conversations.map((conversation, Indx) => (
+          <Conversation
+            key={conversation._id}
+            conversation={conversation}
+            emoji={getRandomEmoji()}
+            lastIndx={Indx === conversations.length - 1}
+          />
+        ))
+      ) : null}
+
+      {loading && <span className="loading loading-spinner"></span>}
     </div>
   );
-}
+};
 
 export default Conversations;
