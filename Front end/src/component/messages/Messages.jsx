5aaeb0c4e513543bd8527777bdc4ac0f51b2e365
Smah-amen <samahamen20001@gmail.com>
2025-02-09 19:40:25 +0200

    refactor MessageContainer and Messages components to integrate conversation state management and fetch messages dynamically

--- Front end/src/component/messages/Messages.jsx
+++ Front end/src/component/messages/Messages.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import Message from './Message'
-import { Scrollbars } from 'react-custom-scrollbars-2';
+import Message from "./Message";
+import useGetMessage from "../../hooks/useGetMessage";
 
+const Messages = () => { 
+  const { messages, loading } = useGetMessage();
+  console.log("messages:", messages);
 
-const Messages = () => {
   return (
-   
-    <div className='px-4 flex-1 overflow-auto message'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/> 
-         <Message/>
-         <Message/>
-        <Message/>
-        <Message/>
-        <Message/> 
-         <Message/>
+    <div className="px-4 flex-1 overflow-auto message">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
     </div>
- 
+  );
+};
 
-  )
-}
-
-export default Messages
+export default Messages;
