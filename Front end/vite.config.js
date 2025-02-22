24bef61bb1c35ab2de3ba0c841c0ba7bd5c4afae
Smah-amen <samahamen20001@gmail.com>
2025-02-22 15:14:01 +0200

    feat: add new home background image and update CSS; adjust Vite server configuration

--- Front end/vite.config.js
+++ Front end/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': { 
+      '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false, 
-
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
