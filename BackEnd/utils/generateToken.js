e8fc888c2932bd37d36ada51ee2f214f27ccb804
Smah-amen <samahamen20001@gmail.com>
2025-02-24 22:47:03 +0200

    feat: extend JWT token expiration from 20 days to 30 days

--- BackEnd/utils/generateToken.js
+++ BackEnd/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "20d",
+    expiresIn: "30d",
   });
 
   res.cookie("jwt", token, {
