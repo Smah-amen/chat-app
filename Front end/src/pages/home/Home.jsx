1faa8f2554b714608788974176449163851d745c
Smah-amen <samahamen20001@gmail.com>
2025-04-04 16:22:54 +0200

    refactor: update layout in Home component for improved responsiveness and styling

--- Front end/src/pages/home/Home.jsx
+++ Front end/src/pages/home/Home.jsx
@@ -4,9 +4,10 @@ import MessageContainer from "../../component/messages/MessageContainer";
 
 const Home = () => {
   return (
-    <div className="flex min-h-[600px] sm:h-[450px] md:h-[550] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-      <Sidebar />
-      <MessageContainer />
+    <div className="flex flex-col sm:flex-row w-full min-h-screen sm:min-h-[450px] md:min-h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar className="w-full sm:w-1/3 lg:w-1/4" />
+
+\      <MessageContainer className="flex-1 min-h-[500px] sm:min-h-[450px] md:min-h-[550px] overflow-auto" />
     </div>
   );
 };
